a949a819d0971621aa78c42410510030da081098
Nils Gustav Stråbø <nst@equinor.com>
2021-02-16 12:39:08 +0100

    misc

--- compute/main.go
+++ compute/main.go
@@ -2,11 +2,50 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"os"
 	"time"
+
+	"github.com/sirupsen/logrus"
 )
 
 func main() {
 	fmt.Println("computing")
-	time.Sleep(time.Duration(5 * time.Second))
+
+	// Writable file
+	f2, err := os.OpenFile("/tmp/mydata2.txt", os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0666)
+	if err != nil {
+		logrus.Errorf("Open: %v", err)
+		//panic(err)
+	}
+	defer f2.Close()
+
+	f2.Write([]byte("hello 2"))
+
+	// No permission
+	f, err := os.OpenFile("/tmp/mydata.txt", os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0666)
+	if err != nil {
+		logrus.Errorf("Open: %v", err)
+	}
+	defer f.Close()
+
+	if _, err = f.Write([]byte("hello")); err != nil {
+		logrus.Errorf("Write: %v", err)
+	}
+
+	az, err := os.Open("/compute/.azure/sp_credentials.json")
+	if err != nil {
+		logrus.Errorf("Open: %v", err)
+	}
+	defer az.Close()
+
+	bytes, err := ioutil.ReadAll(az)
+	if err != nil {
+		logrus.Errorf("Read az: %v", err)
+	} else {
+		logrus.Infof("File az has %v bytes", len(bytes))
+	}
+
+	time.Sleep(10 * time.Minute)
 	fmt.Println("done")
 }
